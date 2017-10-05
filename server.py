dd3be721b037bd8f766ec87b27f01905d7441a95
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-10-05 14:39:29 +0300

    Добавлено количество фильмов для вывода

--- server.py
+++ server.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 
 tmpfile = tempfile.gettempdir()
 cache = FileSystemCache(cache_dir=tmpfile)
+movies_count_to_output = 10
 
 
 @app.route('/')
 def films_list():
     movies_data = cache.get('movies_data')
     if movies_data is None:
-        movies_data = get_movies_data_for_template_engine(10)
+        movies_data = get_movies_data_for_template_engine(movies_count_to_output)
         cache.set('movies_data', movies_data, timeout=60 * 60)
     return render_template('films_list.html', movies=movies_data)
 
