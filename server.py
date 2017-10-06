671615cd08eaea12cf4540f8b274b855ebb0e52b
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-10-06 12:21:46 +0300

    изменены функции для получения инфо о фильмах

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from cinemas import get_movies_data_for_template_engine
+from cinemas import get_movies_data_for_template_engine, get_movies_urls_and_cinemas
 from werkzeug.contrib.cache import FileSystemCache
 import tempfile
 
@@ -14,7 +14,8 @@ movies_count_to_output = 10
 def films_list():
     movies_data = cache.get('movies_data')
     if movies_data is None:
-        movies_data = get_movies_data_for_template_engine(movies_count_to_output)
+        movies_data = get_movies_data_for_template_engine(
+            get_movies_urls_and_cinemas(), movies_count_to_output)
         cache.set('movies_data', movies_data, timeout=60 * 60)
     return render_template('films_list.html', movies=movies_data)
 
